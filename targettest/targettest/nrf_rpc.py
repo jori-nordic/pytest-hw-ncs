5db5ec1096514d17f29720f0fad4b1da8bb64c07
Jonathan Rico <jonathan.rico@nordicsemi.no>
2022-07-20 15:13:38 +0200

    add UART packet handling
    Signed-off-by: Jonathan Rico <jonathan.rico@nordicsemi.no>

--- targettest/targettest/nrf_rpc.py
+++ targettest/targettest/nrf_rpc.py
@@ -2,10 +2,79 @@
 
 # Scratchpad for nRF RPC UART transport
 
+import struct
 import time
 import threading
 import serial
 import cbor2
+
+
+class UARTHeader():
+    _size = 4 + 2 + 1
+    _header = b'UART'
+
+    def __init__(self, length: int, crc: int):
+        self.length = length
+        self.crc = crc
+        self.raw = struct.pack('<4sHB', self._header, self.length, self.crc)
+
+    @classmethod
+    def unpack(cls, packet: bytes):
+        packet = packet[:cls._size]
+        # print(f'header unpack: buf {packet}')
+        try:
+            (header, length, crc) = struct.unpack('<4sHB', packet)
+            if header == cls._header:
+                # print('header unpack success')
+                return cls(length, crc)
+
+        except struct.error:
+            print("Struct error")
+            pass
+
+        print('header unpack failure')
+        return None
+
+    @classmethod
+    def calc_crc(cls, packet: bytes):
+        return 0
+
+    def __repr__(self):
+        return f'len {self.length} crc {self.crc} raw {self.raw}'
+
+
+class UARTPacket():
+    def __init__(self, payload=b''):
+        self.length = len(payload)
+        self.header = UARTHeader(self.length, UARTHeader.calc_crc(payload))
+        self.payload = payload
+        self.raw = self.header.raw + payload
+
+    def __repr__(self):
+        return f'header {self.header} payload {self.payload}'
+
+    @classmethod
+    def unpack(cls, packet: bytes):
+        # print(f'UARTPacket unpack from {packet}')
+        # Called on the whole buffer, containing the header too
+        header = UARTHeader.unpack(packet)
+        packet = packet[header._size:] # Remove the header
+        packet = packet[:header.length] # Remove anything after the advertised length
+        # print(f'header {header} raw {packet}')
+
+        if len(packet) < header.length:
+            # TODO raise EOFError ?
+            print('Packet not complete')
+            return None
+        else:
+            return cls(packet)
+
+# class RPCPacket():
+#     # Meant to do the lookup from cmd/evt enums and create the appropriate classes
+
+# Missing:
+# - rpcpacket class that encodes the header
+# - rpcbuilder? class that encodes/decodes the actual cmd/evt payload
 
 class UARTRPC(threading.Thread):
     DEFAULT_TIMEOUT = 0.001
@@ -28,8 +97,8 @@ class UARTRPC(threading.Thread):
         self._max_recv_byte_count = self.MAX_RECV_BYTE_COUNT
 
         self._rx_buf = b''
-        self._packet_len = 0
-        self._building_header = True
+        self._building = False
+        self._header = None
 
         self._payload_handler = None
 
@@ -91,74 +160,64 @@ class UARTRPC(threading.Thread):
         self._serial.close()
 
     def handle_rx(self, data):
-        if self._building_header:
-            if len(self._rx_buf) == 0 and data[0] != 85:
-                return
+        # Prepend the (just received) data with the remains of the last RX
+        data = self._rx_buf + data
 
-            self._rx_buf += data
+        if not self._building and len(data) >= UARTHeader._size:
+            # Attempt to decode the header
+            self._header = UARTHeader.unpack(data)
+        else:
+            self._building = False
 
-            if len(self._rx_buf) >= self.RPC_HEADER_LENGTH:
-                packet_len = decode_uart_header(data)
-                if packet_len == 0:
-                    return
-                else:
-                    self._building_header = False
-                    self._packet_len = packet_len
+        if self._header is not None:
+            # Header has been decoded
+            # Try to decode the packet
+            if len(data[self._header._size:]) >= self._header.length:
+                packet = UARTPacket.unpack(data)
+                # Process data
+                self._payload_handler(packet)
+                # Consume the data in the RX buffer
+                data = data[self._header._size + self._header.length + 1:]
+                # Reset state
+                self._header = None
+                self._rx_buf = b''
 
-                    # Only keep the payload
-                    self._rx_buf = b''
-                    data = data[self.RPC_HEADER_LENGTH:]
+        self._rx_buf += data
 
-        # Past this point, we want to receive the payload
-        if len(data) == 0:
-            return
+        # TODO: maybe re-trigger handle_rxr
 
-        self._rx_buf = data[:self._packet_len]
-
-        if len(self._rx_buf) == self._packet_len:
-            self._payload_handler(self._rx_buf)
-            self._rx_buf = data[self._packet_len:]
-            self._building_header = True
-
-def decode_uart_header(data):
-    if data[:4] != b'UART':
-        return 0
-
-    length = data[4] + (data[5] << 8)
-
-    # TODO: implement CRC
-    crc = data[6]
-
-    return length
-
-def handle_payload(payload):
-    if payload[0] == 0x04:
+def handle_payload(payload: UARTPacket):
+    if payload.payload[0] == 0x04:
         handshake()
 
-    print(f'Payload {payload.hex(" ")}')
+    print(f'Payload {payload.payload.hex(" ")}')
 
 def handshake():
-    # len lsb, len msb, crc
-    uart_header = b'UART' + bytearray.fromhex('18 00 00')
-
     # Protocol version + RPC group name
     header = bytearray.fromhex('04 00 ff 00 ff')
     version = b'\x00'
     payload = b'nrf_sample_entropy'
 
-    packet = bytearray(uart_header + header + version + payload)
-    print('Send handshake')
-    rpc.send(packet)
+    packet = bytearray(header + version + payload)
+
+    uart_packet = UARTPacket(packet)
+
+    print(f'Send handshake {uart_packet.raw}')
+    rpc.send(uart_packet.raw)
 
 
-rpc = UARTRPC(port='/dev/ttyACM6')
+rpc = UARTRPC(port='/dev/ttyACM2')
 rpc._payload_handler = handle_payload
 rpc.start()
 
 # Send RPC handshake
 # Doesn't use CBOR
+# Doesn't have the workaround u32 val
 handshake_payload = b'nrf_sample_entropy'
 rpc.send(handshake_payload)
+
+# Receive init command
+# CBOR, has w/a u32
 
 # Don't exit immediately
 rpc.join()
