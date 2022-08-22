cdfe7fdf49d6f090d1bfc1084aeca9e4715d34c8
Rico, Jonathan <jonathan.rico@nordicsemi.no>
2022-08-22 16:28:22 +0200

    initialize bluetooth on startup

--- tests/bt_notify/fw/src/main.c
+++ tests/bt_notify/fw/src/main.c
@@ -5,19 +5,28 @@
  */
 
 #include <zephyr/kernel.h>
+#include <bluetooth/bluetooth.h>
 #include <nrf_rpc.h>
 
 #include "rpc_handler.h"
 
+void evt_ready(void);
+void evt_scan_report(void);
+
 void main(void)
 {
 	printk("RPC testing app started [APP Core].\n");
+
+	bt_enable(NULL);
+	printk("bt enabled\n");
 
 	/* Signal to python testcase that we are ready to accept commands. */
 	evt_ready();
 
 	printk("evt_ready sent\n");
 
+	/* TODO: use an actual scan report */
 	k_msleep(2000);
 	evt_scan_report();
+	printk("evt_scan_report sent\n");
 }
