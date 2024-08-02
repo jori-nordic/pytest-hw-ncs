992848018ea022fe440222d54b158ff9549d5c72
Rico, Jonathan <jonathan.rico@nordicsemi.no>
2024-08-02 09:13:48 +0200

    Move RPCPacket into rpc_channel.py
    It was the only user

--- targettest/targettest/rpc_channel.py
+++ targettest/targettest/rpc_channel.py
@@ -4,12 +4,93 @@
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 #
 import time
+import enum
+import struct
 import queue
 import logging
 from targettest.packet_transport.interface import PacketTransport
-from targettest.rpc_packet import RPCPacket, RPCPacketType
 
 LOGGER = logging.getLogger(__name__)
+
+
+class RPCPacketType(enum.IntEnum):
+    INIT = 0
+    INITRSP = enum.auto()
+    CMD = enum.auto()
+    RSP = enum.auto()
+    EVT = enum.auto()
+    ACK = enum.auto()
+    ERR = enum.auto()
+    LOG = enum.auto()
+
+
+def _encode(data: dict):
+    encoded = b''
+    for key, value in data.items():
+        if not isinstance(value[1], bytes):
+            encoded += struct.pack(value[0], value[1])
+        else:
+            encoded += value[1]
+
+    return encoded
+
+def _decode(schema: dict, buf: bytearray):
+    result = {}
+    offset = 0
+    for key, value in schema.items():
+        result[key] = (value[0], struct.unpack_from(value[0], buf, offset)[0])
+        offset += struct.calcsize(value[0])
+
+    return result
+
+
+class RPCPacket():
+    _format = '<BH'
+    _size = struct.calcsize(_format)
+
+    # Header: type + opcode
+    def __init__(self,
+                 packet_type: RPCPacketType,
+                 opcode,
+                 payload: bytes or dict = None):
+        self.packet_type = RPCPacketType(packet_type)
+        self.opcode = opcode
+        self.header = struct.pack(self._format,
+                                  packet_type,
+                                  opcode)
+
+        if isinstance(payload, bytes):
+            self.payload = payload
+        elif isinstance(payload, dict):
+            self.payload = _encode(payload)
+        else:
+            raise Exception("Provide payload as either a dict or bytes")
+
+        # Build whole packet. Transport-specific headers are defined and
+        # prepended (if necessary) by the transports themselves.
+        self.serialized = self.header + self.payload
+
+    def __repr__(self):
+        return '{} {:02x} LEN {} DATA {}'.format(
+            self.packet_type.name,
+            self.opcode,
+            len(self.payload),
+            self.payload.hex(' ')
+        )
+
+    @classmethod
+    def unpack(cls, payload: bytes):
+        # Separate RPC cmd/evt payload from RPC header
+        rpc_header = payload[:cls._size]
+        payload = payload[cls._size:]
+
+        packet_type, opcode = struct.unpack(cls._format, rpc_header)
+
+        return RPCPacket(packet_type, opcode, payload)
+
+
+    def decode(self, schema: dict):
+        return _decode(schema, self.payload)
 
 
 class RPCChannel():
